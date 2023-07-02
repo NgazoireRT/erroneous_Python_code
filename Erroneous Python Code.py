1b76ce75bf9af209c1b167b7087d0b4e13732c7c
Robert Takenzire <81983323+NgazoireRT@users.noreply.github.com>
2023-07-02 11:50:58 +0100

    Add files via upload

---
+++ Erroneous Python Code.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Practical Activity - 1.2.8 - Interpreting and fixing errors
+
+print("My name is Robert Takenzire")
+print("The date is 02/07/2023")
+
+
+# In[3]:
+
+
+#Code 1
+# Print the price.
+sales_price = 24500
+
+print(total_price)
+
+
+# In[4]:
+
+
+# Code 1 - solution [ Replace print(total_price) with print(sales_price)]
+# Print the sales price
+sales_price = 24500
+
+print(sales_price)
+
+
+# In[5]:
+
+
+#Code 2
+
+# Print a text string verbatim.
+print "My name is James Bond"
+
+
+# In[7]:
+
+
+#Code 2 - Solution [add parenthis so looks ] 
+
+print("My Name is James Bond")
+
+
+# In[8]:
+
+
+#Code 3 
+# Determine whether x is greater than 10.
+x = 11 
+
+if x > 10:
+print('X is greater than 10')
+else: 
+    print('x is not greater than 10')
+
+
+# In[10]:
+
+
+#Code 3 - Solution - [ add spaces before the first print function]
+
+x = 11 
+
+if x > 10:
+    print('X is greater than 10')
+else: 
+    print('x is not greater than 10')
+
+
+# In[11]:
+
+
+# Code 4 
+
+# Create the variable list_a.
+list_a = [1,2,3,4,'Ayaan', 'Hirsi']
+
+list_a[11]
+
+
+# In[15]:
+
+
+# Code 4 - Solution  [ there is no index 11 as index starts at 0 and in this list there is 6]
+
+# Create the variable list_a.
+list_a = [1,3,2,4,'Ayaan', 'Hirsi']
+
+print(list_a)
+
+
+# In[ ]:
+
+
+
+
